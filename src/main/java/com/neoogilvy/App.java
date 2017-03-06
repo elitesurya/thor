e748150265dfbab8bf1f9a20c3152960a36a5b70
elitesurya <elitesurya@gmail.com>
2017-03-06 13:08:22 +0530

    Reformat the Code!

--- src/main/java/com/neoogilvy/App.java
+++ src/main/java/com/neoogilvy/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main ( String[] args ) throws InterruptedException
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Hello Surya ");
+        System.out.println( "Hello Surya" );
     }
 }
